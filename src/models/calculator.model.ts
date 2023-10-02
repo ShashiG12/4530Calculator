bb6e44d3ab89618ac4e189e002811c4c2918b960
ShashiG12 <34193029+ShashiG12@users.noreply.github.com>
2023-10-02 17:11:09 -0400

    working tests

--- src/models/calculator.model.ts
+++ src/models/calculator.model.ts
@@ -64,6 +64,60 @@ export class CalculatorModel implements ICalculatorModel {
         //   }
         // }
         // this._buffer = String(currentTotal)
+        const operators: string[] = [];
+        const operands: number[] = [];
+        let currentOperand: string = '';
+
+        for (let i = 0; i < this._buffer.length; i++) {
+          const char = this._buffer[i];
+          if (char === '+' || char === '-' || char === '*' || char === '/') {
+            // Handle operators
+            while (operators.length > 0 && this.hasPrecedence(char, operators[operators.length - 1])) {
+              const operator = operators.pop();
+              const secondOperand = operands.pop();
+              const firstOperand = operands.pop();
+              if (operator === '+') {
+                operands.push(firstOperand + secondOperand);
+              } else if (operator === '-') {
+                operands.push(firstOperand - secondOperand);
+              } else if (operator === '*') {
+                operands.push(firstOperand * secondOperand);
+              } else if (operator === '/') {
+                if (secondOperand === 0) {
+                  // Handle division by zero error.
+                  this._buffer = 'Error';
+                  return;
+                }
+                operands.push(firstOperand / secondOperand);
+              }
+            }
+            operators.push(char);
+          } else if (!isNaN(Number(char))) {
+            // Handle operands
+            currentOperand += char;
+          }
+
+          if (currentOperand !== '') {
+            operands.push(parseFloat(currentOperand));
+            currentOperand = '';
+          }
+        }
+
+        while (operators.length > 0) {
+          const operator = operators.pop();
+          const secondOperand = operands.pop();
+          const firstOperand = operands.pop();
+          if (operator === '+') {
+            operands.push(firstOperand + secondOperand);
+          } else if (operator === '-') {
+            operands.push(firstOperand - secondOperand);
+          } else if (operator === '*') {
+            operands.push(firstOperand * secondOperand);
+          } else if (operator === '/') {
+            operands.push(firstOperand / secondOperand);
+          }
+        }
+        this._buffer = String(operands[0])
        
       default:
         break;
@@ -75,13 +129,13 @@ export class CalculatorModel implements ICalculatorModel {
   }
 
   private hasPrecedence(op1: string, op2: string): boolean {
-    const precedenceMap: { [key: string]: number } = {
+    const precedenceDict: { [key: string]: number } = {
       '+': 1,
       '-': 1,
       '*': 2,
       '/': 2,
     };
-    return precedenceMap[op1] <= precedenceMap[op2];
+    return precedenceDict[op1] <= precedenceDict[op2];
   }
 
   private operation(num1: String, operator: String, num2: String): String {
